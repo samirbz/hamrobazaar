471998b931879f3943b814ecbd003a933a9b320d
samir <febsec14@gmail.com>
2024-04-03 12:20:52 +0545

    sticky fixed

--- components/HomeListing.tsx
+++ components/HomeListing.tsx
@@ -4,7 +4,7 @@ import TabContent from "./TabContent";
 
 const HomeListing = () => {
   return (
-    <div className="flex w-full flex-col px-4 max-xs:px-0">
+    <div className="sticky flex w-full flex-col px-4 max-xs:px-0">
       <HomeHeader />
       <div className="h-px w-full bg-gray-300"></div>
       <div className="pt-4">
