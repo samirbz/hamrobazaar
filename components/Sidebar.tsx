93233889d498dac95f33f2a85a30bc0a5b16a6f0
samir <febsec14@gmail.com>
2024-04-03 13:43:27 +0545

    sidebar scroll done

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,45 +4,49 @@ import SidebarList from "./ui/SidebarList";
 
 const Sidebar = () => {
   return (
-    <div className="h-fit w-[39%] max-md:hidden">
+    <div className="sticky top-20 z-10 h-screen w-[39%]  max-md:hidden">
       {/* SIDEBAR HEADER  */}
-      <div className="flex h-[48px] items-center px-2">
-        <Image
-          src="/icons/menu.svg"
-          width={24}
-          height={24}
-          alt="menu"
-          className="mr-2 size-[24px]"
-        />
-        <h2 className="font-medium">Category</h2>
+      <div className=" bg-white">
+        <div className="flex h-[48px] items-center px-2">
+          <Image
+            src="/icons/menu.svg"
+            width={24}
+            height={24}
+            alt="menu"
+            className="mr-2 size-[24px]"
+          />
+          <h2 className="font-medium">Category</h2>
+        </div>
+        {/* Horizontal line  */}
+        <div className="my-[6px] h-px bg-gray-200"></div>
       </div>
-      {/* Horizontal line  */}
-      <div className="my-[6px] h-px bg-gray-200"></div>
 
       {/* SIDEBAR LIST  */}
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
-      <SidebarList />
+      <div className="hideScrollbar h-screen overflow-y-scroll">
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+        <SidebarList />
+      </div>
     </div>
   );
 };
