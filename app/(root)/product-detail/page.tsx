c6e83e68de06f9313e51b8bf87792726f3c17e83
samir <febsec14@gmail.com>
2024-04-05 22:34:03 +0545

    product detail page pending

--- app/(root)/product-detail/page.tsx
+++ app/(root)/product-detail/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const PrductDetail = () => {
-  return <div>PrductDetail</div>;
+  return (
+    <div className="flex max-w-[1903px] justify-center">
+      <div className="mt-3 flex w-[1296px] max-w-full">
+        <div className="flex-[1.02] p-4">Aside</div>
+        <div className="mx-[6px] h-screen w-px bg-gray-300"></div>
+        <div className="flex-[1.6] px-4">product details</div>
+        <div className="mx-[6px] h-screen w-px bg-gray-300"></div>
+        <div className="my-4 flex-[1.1] pl-4 max-xl:hidden">
+          <span>Similar Product</span>
+          <div className="my-4 h-px  bg-gray-300"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PrductDetail;
