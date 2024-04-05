3819c39ede8a4c2fe6e78897263d6f724ae62492
samir <febsec14@gmail.com>
2024-04-05 20:19:43 +0545

    login page done

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 const Login = () => {
   return (
-    <div className="flex h-[740px] gap-24 bg-[#fafafa] px-3 py-[48px]">
+    <div className="flex h-[740px] gap-24 overflow-auto bg-[#fafafa] px-3 py-[48px] max-lg:h-full max-lg:flex-col-reverse max-lg:gap-[74px] max-lg:py-0 max-sm:pt-2">
       {/* Left Form  */}
-      <div className="flex flex-col gap-12">
-        <div>
+      <div className="flex flex-col gap-12 ">
+        <div className="max-lg:hidden">
           <Image
             alt="search box"
             src="/images/search.gif"
@@ -14,7 +14,7 @@ const Login = () => {
             height={430}
           />
         </div>
-        <div>
+        <div className="max-md:hidden">
           <Image
             alt="playstore"
             src="/images/playstore.png"
@@ -25,10 +25,18 @@ const Login = () => {
       </div>
 
       {/* Right form  */}
-      <div className="flex w-[440px] flex-col gap-12">
+      <div className="flex w-[440px] flex-col max-lg:w-full">
         <div className="flex flex-col">
-          <div className="mb-[32px] flex w-full items-center justify-between rounded-lg bg-[#eeeeee] p-4">
-            <div className="flex h-[92px] flex-col justify-center">
+          <div className="mb-[30px] sm:hidden">
+            <Image
+              alt="logo"
+              src="/icons/half-login-logo.svg"
+              width={30}
+              height={30}
+            />
+          </div>
+          <div className="mb-[32px] flex w-full items-center justify-between rounded-lg bg-[#eeeeee] p-4 max-sm:h-[79px] ">
+            <div className="flex h-[92px] flex-col justify-center ">
               <h2 className="text-[19px] font-medium">For Existing Users</h2>
               <span className="text-[16px] text-[#a5a5a5]">
                 Verify email <span className="text-black">Here</span>
@@ -39,11 +47,21 @@ const Login = () => {
               alt="login image"
               width={140}
               height={92}
+              className="max-sm:hidden"
             />
           </div>
 
           <div className="flex flex-col gap-6">
-            <h2 className="text-[22px] font-medium">Login</h2>
+            <div className="flex justify-between max-sm:items-center">
+              <h2 className="text-[22px] font-medium">Login</h2>
+              <Image
+                src="/images/login-image.svg"
+                alt="login image"
+                width={140}
+                height={92}
+                className="sm:hidden"
+              />
+            </div>
             <div className="flex flex-col ">
               <div className="flex flex-col gap-4">
                 <div className="flex flex-col gap-1">
@@ -52,9 +70,9 @@ const Login = () => {
                     placeholder="Phone Number"
                     className="h-[48px] rounded-lg border border-black px-[6.4px] py-3"
                   />
-                  <label className="mr-[6.4px] text-[13px] text-red-400">
+                  {/* <label className="mr-[6.4px] text-[13px] text-red-400">
                     Phone number is required
-                  </label>
+                  </label> */}
                 </div>
                 <div className="flex flex-col gap-1">
                   <input
@@ -62,9 +80,9 @@ const Login = () => {
                     placeholder="Password"
                     className="h-[48px] rounded-lg border border-black px-[6.4px] py-3"
                   />
-                  <label className="mr-[6.4px] text-[13px] text-red-400">
+                  {/* <label className="mr-[6.4px] text-[13px] text-red-400">
                     Password is required
-                  </label>
+                  </label> */}
                 </div>
               </div>
               <div className="my-[32px] h-px w-full bg-gray-300"></div>
@@ -78,9 +96,10 @@ const Login = () => {
             </div>
           </div>
         </div>
-        <div className="h-[134px] ">
+        <div className="py-9 ">
           <span className="my-[9px]  text-[13px]">
-            Don&apos;t have an account?Sing Up
+            Don&apos;t have an account?{" "}
+            <span className="underline">Sign Up</span>
           </span>
         </div>
       </div>
