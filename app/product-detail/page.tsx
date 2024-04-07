9de565fe0955e08c23bd1bff0ac9088c6900597c
samir <febsec14@gmail.com>
2024-04-07 21:01:23 +0545

    maintainance

--- app/product-detail/page.tsx
+++ app/product-detail/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const PrductDetail = () => {
-  return <div>Product detail</div>;
+  return (
+    <div className="flex max-w-[1903px] justify-center">
+      <div className="flex w-[1320px] max-w-full flex-col px-3 max-md:pt-2 ">
+        Product detail
+      </div>
+    </div>
+  );
 };
 
 export default PrductDetail;
