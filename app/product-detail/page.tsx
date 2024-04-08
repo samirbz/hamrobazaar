d5b9d5c4d23b0cb0264a312528a2dfb4c81714fd
samir <febsec14@gmail.com>
2024-04-08 09:58:26 +0545

    product detail page pending

--- app/product-detail/page.tsx
+++ app/product-detail/page.tsx
@@ -1,10 +1,143 @@
+import AdCard from "@/components/ui/cards/AdCard";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const PrductDetail = () => {
   return (
-    <div className="flex max-w-[1903px] justify-center">
-      <div className="flex w-[1320px] max-w-full flex-col px-3 max-md:pt-2 ">
-        Product detail
+    <div className="mt-3 flex max-w-[1920px] justify-center ">
+      <div className="flex w-[1320px] max-w-full px-3 max-md:pt-2 ">
+        {/* Product  */}
+        <div className="flex w-[362px] flex-col p-4">
+          <div className="flex flex-col gap-6">
+            <Image
+              src="https://picsum.photos/id/2/331/270"
+              width={331}
+              height={270}
+              alt="Image"
+              className="rounded-lg"
+            />
+            <div className="flex items-center justify-between">
+              <div className="flex  gap-4">
+                <Image
+                  src="https://picsum.photos/id/2/32/32"
+                  width={32}
+                  height={32}
+                  alt="Image"
+                  className="rounded-md"
+                />
+                <Image
+                  src="https://picsum.photos/id/2/32/32"
+                  width={32}
+                  height={32}
+                  alt="Image"
+                  className="rounded-md"
+                />
+              </div>
+              <span>Rs. 20000</span>
+            </div>
+          </div>
+          <div className="mt-2 flex justify-between">
+            <span className="text-sm">0 Views</span>
+            <span className="text-[11px]">Like New</span>
+          </div>
+          <div className="my-4 flex h-[48px] items-start gap-4">
+            <Image
+              src="https://picsum.photos/id/200/40/40"
+              width={40}
+              height={40}
+              alt="Image"
+              className="rounded-full"
+            />
+            <div className="">
+              <div className="flex items-center">
+                <span className="mr-2">Sameer bajracharya</span>
+                <div className="mr-3 h-4 w-px bg-black"></div>
+                <span className="text-xs">7 Ads</span>
+              </div>
+              <div>
+                <span className="ml-[6.4px] text-[13px] text-gray-400">
+                  9803260827
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className=" h-px w-full bg-gray-300"></div>
+          <div className="m-[14px] flex items-center p-[14px]">
+            <span className="px-[14px]">Save</span>
+            <div className="mr-3 h-4 w-px bg-black"></div>
+
+            <span className="px-[14px]">Chat Now</span>
+          </div>
+          <span className="text-sm text-gray-500">
+            Note: We recommend you to physically inspect the product/ Service
+            before making payment. Avoid paying fees or advance payment to
+            sellers. Click here for more details
+          </span>
+        </div>
+        <div className="mx-[6px] h-screen w-px bg-gray-300"></div>
+
+        {/* Product Detail  */}
+        <div className="w-[538px] px-4">
+          <h1 className="mb-[11px] mt-[20px] text-2xl font-semibold ">
+            Macbook pro Apple laptop
+          </h1>
+          <div className="flex gap-4">
+            <span className="pb-[11px] pl-[11px] pr-[5px]">Description</span>
+            <span className="pb-[11px] pl-[11px] pr-[5px]">Comments</span>
+            <span className="pb-[11px] pl-[11px] pr-[5px]">location</span>
+          </div>
+          <div className="h-px w-full bg-gray-300"></div>
+          <div className="flex flex-col gap-2 pt-4">
+            <div className="flex flex-col">
+              <span className="">9803260827</span>
+              <span className="mb-4">Bike condition ramro xa</span>
+            </div>
+            <div className="my-[24px]">
+              <span className="mb-[21px] text-[18px]">General</span>
+              <div className="h-[222px] bg-[#FAF9FA]">
+                <div className="flex gap-28">
+                  <span>AD ID</span>
+                  <span>HB-B8C71C</span>
+                </div>
+
+                <div className="flex gap-28">
+                  <span>Location</span>
+                  <span>All Core Lalitpur, Lalitpur</span>
+                </div>
+              </div>
+              <Image
+                src="/ad/ad-productdetail-down.jpeg"
+                width={506}
+                height={111}
+                alt="Image"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mx-[6px] h-screen w-px bg-gray-300"></div>
+
+        {/* Similar product  */}
+        <div className="my-4 w-[362px] pl-4">
+          <p>Similar Product</p>
+          <div className="my-4 h-px w-full bg-gray-300"></div>
+          <div className="flex gap-2">
+            <AdCard />
+            <AdCard />
+          </div>
+          <div className="mb-4 flex justify-center">
+            <Image
+              src="/ad/ad-productdetail.jpeg"
+              width={250}
+              height={250}
+              alt="Image"
+            />
+          </div>
+          <div className="flex gap-2">
+            <AdCard />
+            <AdCard />
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ components/ui/cards/AdCard.tsx
@@ -0,0 +1,76 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const AdCard = () => {
+  return (
+    <Link
+      href="product-detail"
+      className="mb-4 flex h-[277px] w-[166px] cursor-pointer flex-col  gap-3 rounded-md p-2 transition-all duration-700 hover:rounded-lg hover:border hover:border-[#0170b4] hover:bg-[rgba(1,112,180,.08)] "
+    >
+      <Image
+        src="https://picsum.photos/id/5/148/140"
+        width={148}
+        height={140}
+        alt="image"
+        className="rounded-lg"
+      />
+
+      <div className="flex">
+        <h3 className=" text-[14px] font-medium leading-4">
+          Land Cruiser prado
+        </h3>
+
+        <span>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12.0001 7.5C12.8285 7.5 13.5001 6.82843 13.5001 6C13.5001 5.17157 12.8285 4.5 12.0001 4.5C11.1717 4.5 10.5001 5.17157 10.5001 6C10.5001 6.82843 11.1717 7.5 12.0001 7.5Z"
+              fill="black"
+            ></path>
+            <path
+              d="M12.0001 13.5C12.8285 13.5 13.5001 12.8284 13.5001 12C13.5001 11.1716 12.8285 10.5 12.0001 10.5C11.1717 10.5 10.5001 11.1716 10.5001 12C10.5001 12.8284 11.1717 13.5 12.0001 13.5Z"
+              fill="black"
+            ></path>
+            <path
+              d="M12.0001 19.5C12.8285 19.5 13.5001 18.8284 13.5001 18C13.5001 17.1716 12.8285 16.5 12.0001 16.5C11.1717 16.5 10.5001 17.1716 10.5001 18C10.5001 18.8284 11.1717 19.5 12.0001 19.5Z"
+              fill="black"
+            ></path>
+          </svg>
+        </span>
+      </div>
+      <p className="text-xs">Used</p>
+      <div className="flex gap-2">
+        <h2 className="text-xs">Rs.1,70,00,000</h2>
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M11.2133 1.33333H4.78667C3.36667 1.33333 2.21333 2.49333 2.21333 3.90667V13.3C2.21333 14.5 3.07333 15.0067 4.12667 14.4267L7.38 12.62C7.72667 12.4267 8.28667 12.4267 8.62667 12.62L11.88 14.4267C12.9333 15.0133 13.7933 14.5067 13.7933 13.3V3.90667C13.7867 2.49333 12.6333 1.33333 11.2133 1.33333Z"
+            stroke="black"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+          <path
+            d="M9.66667 7.1H6.33333M8 5.47334V8.80667"
+            stroke="black"
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+        </svg>
+      </div>
+    </Link>
+  );
+};
+
+export default AdCard;
